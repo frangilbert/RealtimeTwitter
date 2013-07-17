9eddc2857d00c89e2b9c093cdea40d55d5c6241c
Francis Gilbert <francis.gilbert@akqa.com>
2013-07-17 10:11:40 +0100

    Adding files for Twitter Node Prototype.

---
+++ routes/realtime.js
@@ -0,0 +1,22 @@
+redis = require("redis");
+
+exports.connect = function (req, res) {
+    client = redis.createClient();
+    console.log('Working');
+    
+    client.on("error", function (err) {
+        console.log("Error " + err);
+    });
+
+    client.set("string key", "string val", redis.print);
+    client.hset("twitter", "hashtest 1", "some value");
+    client.hset(["twitter", "hashtest 2", "some other value"], redis.print);
+    client.hgetall("twitter", function (err, obj) {
+            console.dir(obj);
+        });
+        client.quit();
+    
+    res.locals.test = 'hello?';
+
+    res.send(res.locals.test);
+};
